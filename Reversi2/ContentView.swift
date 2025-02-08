649332b723c36d818711b81ad720ed0e4e45518e
Stephen Tim <571268@qq.com>
2025-02-08 11:02:15 +0800

    Initial Commit

---
+++ Reversi2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reversi2
+//
+//  Created by Stephen Tim on 2025/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reversi2/Reversi2App.swift
@@ -0,0 +1,17 @@
+//
+//  Reversi2App.swift
+//  Reversi2
+//
+//  Created by Stephen Tim on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct Reversi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
